520cc26d71ab8701b14f768985a515f0e2d55b0b
Maryadi <maryadi@elephanttalk.com>
2016-06-09 13:55:01 +0700

    Kode awal array inversion... :D

--- src/hisoka/poipo/MainCodility.java
+++ src/hisoka/poipo/MainCodility.java
@@ -4,9 +4,28 @@ public class MainCodility {
 
 	public static void main(String[] args) {
 		
-		
+		int a = 2147483647;
 		System.out.println(Integer.MAX_VALUE);
+		System.out.println(a);
+		
 	}
+	
+	//ArrayInversionCount
+	//https://codility.com/programmers/task/array_inversion_count/
+	public static int arrInvCount(int[] A)
+	{
+		int count = 0;
+		for(int p=0; p<A.length; p++)
+		{
+			for(int q=p+1; q<A.length; q++)
+			{
+				if(A[p]>A[q]) count++;
+			}
+		}
+		if(count > 1000000000) return -1;
+		return count;
+	}
+	
 	
 	
 	//str symetry point
@@ -27,8 +46,7 @@ public class MainCodility {
 	}
 	
 	
-	//ArrayInversionCount
-	//https://codility.com/programmers/task/array_inversion_count/
+	
 	
 	
 
